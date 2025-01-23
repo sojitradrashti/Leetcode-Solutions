8560c4b1b9bed3ed3eed5da66bd7dc61a797c4ee
Drashti Sojitra <137138587+sojitradrashti@users.noreply.github.com>
2025-01-23 10:18:51 +0530

    Update 3_Sum.py

--- 3_Sum.py
+++ 3_Sum.py
@@ -1,15 +1,14 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        result=[]
+        result=set()
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 for k in range(j+1,len(nums)):
                     if nums[i]+ nums[j]+ nums[k] == 0:
                         list1=[nums[i],nums[j],nums[k]]
-                        if list1 not in result:
-                            result.append(list1)    
-        return result
+                        result.add(tuple(list1))    
+        return list(result)
 nums=[-1,0,1,2,-1,-4]
 r=Solution()
 x= r.threeSum(nums)
