2d99b9ee8cdc683802c2755bf83a853e8902cf56
Drashti Sojitra <137138587+sojitradrashti@users.noreply.github.com>
2025-01-24 14:54:44 +0530

    Create 55_Jump Game.py

---
+++ 55_Jump Game.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        maxJump=0
+        for i in range(len(nums)):
+            for j in range(1,len(nums)):
+                if i > maxJump:
+                    return False
+                maxJump=max(maxJump,i+nums[i])
+
+                if maxJump >= len(nums)-1:
+                    return True
+        return False
+nums=[2,3,1,1,4]
+solution=Solution()
+result=solution.canJump(nums)
+print(result)
