d30290465a288b550a580c9d1a7f283ba9ef2d1c
Drashti Sojitra <137138587+sojitradrashti@users.noreply.github.com>
2025-01-24 17:01:10 +0530

    Create 136_Single Number.py

---
+++ 136_Single Number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result=None
+        for i in range(len(nums)):
+            result1=nums.count(nums[i])
+            if result1 == 1:
+                result=nums[i]
+        return result
+nums=[2,2,1]
+solution=Solution()
+ans = solution.singleNumber(nums)
+print(ans)
